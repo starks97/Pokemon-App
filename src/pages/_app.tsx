b07c011a506cac7f241f0945e1ef3a252111fc6e
Jefferson <ifrit68@hotmail.com>
2022-05-22 22:18:11 -0400

    [seating all the environment for the project]

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { NextUIProvider } from "@nextui-org/react";
+import type { AppProps } from "next/app";
+import { darkTheme } from "../themes";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Button } from "@nextui-org/react";
+
+const Home: NextPage = () => {
+  return <Button color="gradient">Hey How its going</Button>;
+};
+
+export default Home;
